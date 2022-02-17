eb44c3b19852de881742669cde8affebc4ae15b8
Peter Arwanitis <peter.arwanitis@cognite.com>
2022-02-17 12:30:57 +0100

    update to 3.9 and manually set version

--- incubator/bootstrap_cli/__init__.py
+++ incubator/bootstrap_cli/__init__.py
@@ -1,2 +1,9 @@
 # version
-__version__ = "1.2.0"
+
+# requires py>3.7
+# from importlib_metadata import version as pyproject_version
+# __version__ = pyproject_version("inso-extpipes-cli")
+# print(f'__version__: {__version__}')
+
+# keep manually in sync with pyproject.toml until the above approach is working in Docker too
+__version__ = '1.2.1'
