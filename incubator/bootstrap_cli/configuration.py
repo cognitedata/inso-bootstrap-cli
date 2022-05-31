eee06e6b63c493384a247f674cebd4bcf5f611af
Peter Arwanitis <peter.arwanitis@cognite.com>
2022-05-31 12:56:46 +0200

    added figlets (banner) comments - easy navigation for mini-preview

--- incubator/bootstrap_cli/configuration.py
+++ incubator/bootstrap_cli/configuration.py
@@ -6,6 +6,34 @@ from cognite.extractorutils.configtools import CogniteConfig, LoggingConfig, loa
 from dotenv import load_dotenv
 
 
+# mixin 'str' to 'Enum' to support comparison to string-values
+# https://docs.python.org/3/library/enum.html#others
+# https://stackoverflow.com/a/63028809/1104502
+class YesNoType(str, Enum):
+    yes = "yes"
+    no = "no"
+
+
+class CommandMode(str, Enum):
+    PREPARE = "prepare"
+    DEPLOY = "deploy"
+    DELETE = "delete"
+    DIAGRAM = "diagram"
+
+
+# '''
+#                                              888    d8b
+#                                              888    Y8P
+#                                              888
+#   .d88b.  888  888  .d8888b .d88b.  88888b.  888888 888  .d88b.  88888b.  .d8888b
+#  d8P  Y8b `Y8bd8P' d88P"   d8P  Y8b 888 "88b 888    888 d88""88b 888 "88b 88K
+#  88888888   X88K   888     88888888 888  888 888    888 888  888 888  888 "Y8888b.
+#  Y8b.     .d8""8b. Y88b.   Y8b.     888 d88P Y88b.  888 Y88..88P 888  888      X88
+#   "Y8888  888  888  "Y8888P "Y8888  88888P"   "Y888 888  "Y88P"  888  888  88888P'
+#                                     888
+#                                     888
+#                                     888
+# '''
 class BootstrapConfigError(Exception):
     """Exception raised for config parser
 
@@ -30,19 +58,16 @@ class BootstrapValidationError(Exception):
         super().__init__(self.message)
 
 
-# mixin 'str' to 'Enum' to support comparison to string-values
-# https://docs.python.org/3/library/enum.html#others
-# https://stackoverflow.com/a/63028809/1104502
-class YesNoType(str, Enum):
-    yes = "yes"
-    no = "no"
-
-
-class CommandMode(str, Enum):
-    PREPARE = "prepare"
-    DEPLOY = "deploy"
-    DELETE = "delete"
-    DIAGRAM = "diagram"
+# '''
+#       888          888                      888
+#       888          888                      888
+#       888          888                      888
+#   .d88888  8888b.  888888  8888b.   .d8888b 888  8888b.  .d8888b  .d8888b
+#  d88" 888     "88b 888        "88b d88P"    888     "88b 88K      88K
+#  888  888 .d888888 888    .d888888 888      888 .d888888 "Y8888b. "Y8888b.
+#  Y88b 888 888  888 Y88b.  888  888 Y88b.    888 888  888      X88      X88
+#   "Y88888 "Y888888  "Y888 "Y888888  "Y8888P 888 "Y888888  88888P'  88888P'
+# '''
 
 
 @dataclass
